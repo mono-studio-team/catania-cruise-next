7a8471d9412209bbf98b52acef2b38632bbef943
simona-mono <simona@mono.studio>
2025-05-15 17:39:29 +0200

    chore: new deps

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
 }
 
