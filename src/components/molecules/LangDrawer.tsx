723a97c0db5f7712bb9cfe1be0212ef6952dca87
simona-mono <simona@mono.studio>
2025-05-15 18:17:37 +0200

    fix: drawer ssr mismatch err

--- src/components/molecules/LangDrawer.tsx
+++ src/components/molecules/LangDrawer.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { useDrawer } from '@/contexts/DrawerContext'
-import Drawer from 'react-modern-drawer'
+import dynamic from 'next/dynamic'
 import 'react-modern-drawer/dist/index.css'
+const Drawer = dynamic(() => import('react-modern-drawer'), { ssr: false })
 
 export default function LangDrawer() {
   const { isDrawerOpen, closeDrawer } = useDrawer()
 
   return (
-    <Drawer customIdSuffix="lang-switch-drawer" open={isDrawerOpen} onClose={closeDrawer} direction="bottom">
+    <Drawer customIdSuffix="lang-switch-drawer886" open={isDrawerOpen} onClose={closeDrawer} direction="bottom">
       <div className="p-4">
         <h3 className="text-xl font-semibold">Language Switcher</h3>
         {/* TODO - Language selection here */}
