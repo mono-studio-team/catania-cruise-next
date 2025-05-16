008e5890d5b203d90edc633664f86723ab3850bc
simona-mono <simona@mono.studio>
2025-05-16 16:28:57 +0200

    feat: route-points section

---
+++ src/components/organisms/RoutePointsSection.tsx
@@ -0,0 +1,17 @@
+import { RoutePoint } from '@/types/api'
+import Stepper from '../molecules/Stepper'
+import RoutePointCard from '../molecules/RoutePointCard'
+
+type RoutePointsSectionProps = {
+  points?: RoutePoint[]
+}
+
+export default function RoutePointsSection({ points = [] }: RoutePointsSectionProps) {
+  const steps = points.map((point, index) => <RoutePointCard key={point.id} index={index} title={point.name.value} />)
+
+  return (
+    <div className="mt-10 px-5 pb-22">
+      <Stepper steps={steps} />
+    </div>
+  )
+}
