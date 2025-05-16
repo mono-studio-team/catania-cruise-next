4361c620f9632f638b02e24268f5ddc1c6944a98
simona-mono <simona@mono.studio>
2025-05-16 10:53:46 +0200

    feat: home routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+
+import { getAllRoutes } from '@/api/entities'
+import HomeHeader from '@/components/organisms/HomeHeader'
+import RoutesSection from '@/components/organisms/RoutesSection'
+import { Route } from '@/types/api'
+import { useEffect, useState } from 'react'
+
 export default function Home() {
+  const [routes, setRoutes] = useState<Route[]>([])
+
+  useEffect(() => {
+    const fetchRoutes = async () => {
+      try {
+        const data = await getAllRoutes()
+        console.log('Fetched routes:', data)
+        setRoutes(data)
+      } catch (err) {
+        console.error('Error fetching routes:', err)
+      }
+    }
+
+    fetchRoutes()
+  }, [])
+
   return (
-    <div>
-      <h1>Hello Next.js!</h1>;
+    <div className="px-5 pt-15 pb-22">
+      <HomeHeader />
+      <RoutesSection title="Tutti i percorsi" routes={routes} />
     </div>
   )
 }
