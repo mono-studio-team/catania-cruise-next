310447f0a73b9a3e64cbe9fde569605589d9d80e
simona-mono <simona@mono.studio>
2025-05-16 10:53:32 +0200

    feat: dynamic route section

--- src/components/molecules/RouteCard.tsx
+++ src/components/molecules/RouteCard.tsx
@@ -1,36 +1,46 @@
+'use client'
+
 import Image from 'next/image'
+import Link from 'next/link'
 import Background from '@/assets/example.png'
 import Clock from '@/assets/icons/clock.svg'
 import Flag from '@/assets/icons/km1.svg'
 import Bicicle from '@/assets/icons/bicicle.svg'
+import { Route } from '@/types/api'
 
-export default function RouteCard() {
+type RouteCardProps = {
+  route: Route
+}
+
+export default function RouteCard({ route }: RouteCardProps) {
   return (
-    <div className="rounded-xl w-full px-2 py-2 text-white bg-primary-red">
-      <div className="rounded-lg overflow-hidden h-[134px]">
-        <Image src={Background} alt="Picture of the author" className="object-cover w-full h-full" />
-      </div>
-      <div className="flex justify-between items-center pt-2">
-        <div>
-          <p className="inline-flex items-center gap-1 mb-1.5 font-light text-sm">
-            <span>
-              <Bicicle />
-            </span>
-            Percorso in bicicletta
-          </p>
-          <h3 className="font-medium text-xl">Nel cuore di Catania</h3>
+    <Link href={`/routes/${encodeURIComponent(route.id)}`} passHref>
+      <div className="rounded-xl w-full px-2 py-2 text-white bg-primary-red cursor-pointer hover:opacity-90 transition-opacity">
+        <div className="rounded-lg overflow-hidden h-[134px]">
+          <Image src={Background} alt="Route image" className="object-cover w-full h-full" />
         </div>
-        <div className="text-sm flex gap-1">
-          <div className="flex flex-col justify-center gap-2">
-            <Clock />
-            <Flag />
+        <div className="flex justify-between items-center pt-2 px-1">
+          <div>
+            <p className="inline-flex items-center gap-1 mb-1.5 font-light text-sm">
+              <span>
+                <Bicicle />
+              </span>
+              Percorso in bicicletta
+            </p>
+            <h3 className="font-medium text-xl max-w-64">{route.nameIT.value}</h3>
           </div>
-          <div className="flex flex-col gap-1 items-end">
-            <p>2,5 h</p>
-            <p>18 Km</p>
+          <div className="text-sm mt-auto flex gap-1 shrink-0">
+            <div className="flex flex-col justify-center gap-3">
+              <Clock />
+              <Flag />
+            </div>
+            <div className="flex flex-col gap-2 items-end">
+              <p>2,5 h</p>
+              <p>18 Km</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
--- src/components/organisms/RoutesSection.tsx
+++ src/components/organisms/RoutesSection.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { useState } from 'react'
 import { Route } from '@/types/api'
 import RouteCard from '../molecules/RouteCard'
-import Button from '../atoms/Button'
-import LangDrawer from '../molecules/LangDrawer'
 
 type RoutesSectionProps = {
   title: string
@@ -12,23 +9,14 @@ type RoutesSectionProps = {
 }
 
 export default function RoutesSection({ title, routes }: RoutesSectionProps) {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-
-  const toggleDrawer = () => {
-    setIsDrawerOpen((prev) => !prev)
-  }
-
   return (
     <div className="mt-10">
       <h2 className="font-medium text-xl mb-5">{title}</h2>
       <div className="flex flex-col gap-4">
         {routes.map((route) => (
-          <RouteCard key={route.id} /> //route={route}
+          <RouteCard key={route.id} route={route} />
         ))}
       </div>
-      <Button onClick={toggleDrawer}>click</Button>
-      {/* Drawer to switch languages */}
-      <LangDrawer isOpen={isDrawerOpen} onClose={toggleDrawer} />
     </div>
   )
 }
