39770d26ef1ebb6d1eb09464ca00cdfc06e388f2
simona-mono <simona@mono.studio>
2025-05-16 10:53:58 +0200

    feat: route details page

---
+++ src/app/routes/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { getEntityById } from '@/api/entities'
+import DetailsHeader from '@/components/organisms/DetailsHeader'
+import { Route } from '@/types/api'
+import { notFound } from 'next/navigation'
+
+type RouteDetailsProps = {
+  params: { id: string }
+}
+
+export default async function RouteDetailsPage({ params }: RouteDetailsProps) {
+  const { id } = await params
+  const data = await getEntityById(decodeURIComponent(id))
+
+  if (!data || data.type !== 'Route') {
+    notFound()
+  }
+
+  const route = data as Route
+  console.log(route)
+  return (
+    <div>
+      <DetailsHeader title={route.nameIT.value} />
+    </div>
+  )
+}
