11406e7b338a48475e0ebc0f2946cfcaec60d0e9
simona-mono <simona@mono.studio>
2025-05-16 16:28:17 +0200

    feat: redirect page

---
+++ src/app/redirect/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Image from 'next/image'
+import Background from '@/assets/login-bg.jpg'
+import LogoCard from '@/components/molecules/LogoCard'
+
+export default function Redirect() {
+  return (
+    <div className="relative h-screen flex items-center justify-center px-5">
+      <Image src={Background} alt="Route image" className="absolute -z-40 object-cover w-full h-full" />
+      <LogoCard />
+    </div>
+  )
+}
