f3ce57b956684153e645bf86d024eac3496afe2c
simona-mono <simona@mono.studio>
2025-05-19 10:16:12 +0200

    feat: locale routing

---
+++ src/i18n/routing.ts
@@ -0,0 +1,17 @@
+import { defineRouting } from 'next-intl/routing'
+
+/*
+  TODO: Observe behavior in different browsers and verify if it will fallback on defaultLocale
+  When localeDetection is enabled, the locale will be detected from the Accept Language header.
+  However, even if the Accept Language contains it, it will fall back to english.
+  Observe behavior in different browsers and devices and eventually add localeDetection: false.
+*/
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ['en', 'it'],
+
+  // Used when no locale matches
+  defaultLocale: 'it',
+})
+
+export type Locale = typeof routing.defaultLocale
