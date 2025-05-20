c8bd1f9019704f40e60af8ae933cfa681de4edfe
simona-mono <simona@mono.studio>
2025-05-20 11:26:23 +0200

    feat: btn dynamic size

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 type ButtonProps = {
   variant?: 'primary' | 'secondary' | 'link'
+  size?: 'small' | 'medium' | 'large'
   type?: 'button' | 'submit' | 'reset'
+  className?: string
   disabled?: boolean
   children: React.ReactNode
   onClick?: () => void
@@ -13,13 +15,21 @@ export default function Button({
   children,
   onClick,
   variant = 'primary',
+  size = 'medium',
   type = 'button',
+  className,
   disabled = false,
 }: ButtonProps) {
   const variants = {
-    primary: 'bg-primary-black text-white rounded-full px-4 py-3',
-    secondary: 'bg-primary-aqua text-white text-lg rounded-t-xl px-4 py-4.5',
+    primary: 'bg-primary-black text-white rounded-full',
+    secondary: 'bg-primary-aqua text-white text-lg rounded-t-xl',
     link: 'text-black underline underline-offset-2 font-semibold decoration-2',
+  }
+
+  const sizes = {
+    small: 'px-7 py-2.5',
+    medium: 'px-4 py-3',
+    large: 'px-4 py-4.5',
   }
 
   const disabledStyle = 'opacity-50 cursor-not-allowed'
@@ -29,8 +39,10 @@ export default function Button({
       type={type}
       onClick={onClick}
       className={clsx(
-        'inline-flex items-center w-full justify-center font-medium cursor-pointer', //TODO - change width
+        'inline-flex items-center w-full justify-center font-medium cursor-pointer',
         variants[variant],
+        sizes[size],
+        className,
         disabled && disabledStyle,
       )}
       disabled={disabled}
